e1ffca193f1f1d7e55f01e06a4934638881a6bba
Jose Smith Marmolejos Rocha <jsmr04@gmail.com>
2021-02-23 15:05:11 -0500

    sign up

---
+++ js/authentication.js
@@ -0,0 +1,69 @@
+const email = document.getElementById("inputEmail");
+const password = document.getElementById("inputPassword");
+const confirmPassword = document.getElementById("inputConfirmPassword");
+const firstName = document.getElementById("inputFirstName");
+const lastName = document.getElementById("inputLastName");
+const errorMesssage = document.getElementById("errorMessage");
+
+//Prevent page reload
+let form = document.getElementById("form-register");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+let user;
+function createNewAccount() {
+  errorMesssage.innerHTML = '';
+  errorMesssage.setAttribute('hidden',true);
+  if (validateFields()) {
+    console.log(`createNewAccount ${email.value} ${password.value}` );
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email.value, password.value)
+      .then((userCredential) => {
+        // Signed in
+        user = {
+            email: userCredential.user.email,
+            firstName: firstName.value,
+            lastName: firstName.value,
+            type: 'customer',
+        }
+        
+        //console.log(userCredential.user)
+        //Storing user information
+        firebase
+        .database()
+        .ref(`userInfo/${userCredential.user.uid}`)
+        .set(user)
+        .then(() =>{
+            //window.location.href = './questions-quiz.html?firstName=' + firstName.value + '&lastName=' + lastName.value + '&email=' + email.value;
+            window.location.href = `./customer-info.html?email=${email.value}`;
+        });
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Error');
+        console.log(errorMessage);
+
+        errorMesssage.innerHTML = errorMessage;
+        errorMesssage.removeAttribute('hidden');
+        // ..
+      });
+  }
+
+  return false;
+}
+
+function validateFields() {
+  if (
+    email.value != "" &&
+    password.value != "" &&
+    confirmPassword.value != "" &&
+    firstName.value != "" &&
+    lastName.value != ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- js/product.js
+++ js/product.js
@@ -233,6 +233,7 @@ function saveProduct() {
 
     if (id != "" && name != "" && price != "" && stock != "") {
         const product = new Product(
+            
             id,
             name,
             description,
@@ -281,8 +282,8 @@ function showPictureName() {
         let spanElement = document.createElement("span");
 
         let iRemove = document.createElement("i");
-        iRemove.setAttribute("class", "fas fa-trash-alt");
-        iRemove.setAttribute("style", "margin-left:2px; font-size: 1rem; color: #5a2d82;");
+        iRemove.setAttribute("class", "bi-backspace-fill");
+        iRemove.setAttribute("style", "margin-left:2px; font-size: 1rem; color: cornflowerblue;");
 
         //Adding event
         iRemove.setAttribute("onclick", `removePicture(${index})`);
@@ -322,12 +323,12 @@ function updateStatus() {
         console.log('Changing status');
         console.log(products[productIndex]);
 
-        if (products[productIndex].pictures == undefined) {
+        if (products[productIndex].pictures == undefined){
             products[productIndex].pictures = [];
         }
-
+        
         products[productIndex].category = getCategoryId(products[productIndex].category);
-
+        
         firebase
             .database()
             .ref(`product/${products[productIndex].id}`)
