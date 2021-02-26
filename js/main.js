01caab6e905f8d60af18e8d301ac341c3b59e912
Arthur Ataide <arthurreisataide@gmail.com>
2021-02-25 22:34:34 -0500

    Banner fixed.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let paramClass;
 let paramCategory;
 let paramPrductName;
 let paramProductId;
+let type;
 
 //Getting params
 if (urlParams.has("classification")) {
@@ -35,6 +36,7 @@ function getCategories() {
 
     fillClassificationDropdown();
     fillLeftPanel();
+    fillBanners();
   });
 }
 
@@ -66,24 +68,30 @@ function fillClassificationDropdown() {
   });
 }
 
+
 function fillLeftPanel() {
   let classification;
   let leftPanelTitle = document.getElementById("leftPanelTitle");
   let leftPanelList = document.getElementById("leftPanelList");
   let title;
 
-  //Women is default
-  classification = paramClass == undefined ? "women" : paramClass;
-  console.log(classification);
+  //All is default
+  classification = paramClass == undefined ? "all" : paramClass;
   switch (classification) {
     case "women":
       title = "Women Clothes";
+      type = "women";
       break;
     case "men":
       title = "Men Clothes";
+      type = "men";
       break;
     case "kids":
       title = "Kids Clothes";
+      type = "kids";
+      break;
+    case "all":
+      title = "All Clothes";
       break;
   }
 
@@ -103,6 +111,49 @@ function fillLeftPanel() {
 
     leftPanelList.appendChild(a);
   });
+}
+
+function fillBanners(){
+  //<img class="d-block img-fluid" src="media/banners/women-banner-1.jpg" alt="First slide"></img>
+    let div1 = document.getElementById("bannerFirstImg");
+    let div2 = document.getElementById("bannerSecondImg");
+    let div3 = document.getElementById("bannerThirdImg");
+
+    if(type != undefined){
+      let image1 = document.createElement("img");
+      image1.setAttribute("class", "d-block img-fluid");
+      image1.setAttribute(
+        "src",
+        `media/banners/${type}/1.jpg`
+      );
+      image1.setAttribute("alt", "First Image");
+
+      let image2 = document.createElement("img");
+      image2.setAttribute("class", "d-block img-fluid");
+      image2.setAttribute(
+        "src",
+        `media/banners/${type}/2.jpg`
+      );
+      image2.setAttribute("alt", "Second Image");
+
+      let image3 = document.createElement("img");
+      image3.setAttribute("class", "d-block img-fluid");
+      image3.setAttribute(
+        "src",
+        `media/banners/${type}/3.jpg`
+      );
+      image3.setAttribute("alt", "Third Image");
+
+      let divider = document.createElement("hr");
+      let div4divider = document.getElementById("divider");
+
+      div1.appendChild(image1);
+      div2.appendChild(image2);
+      div3.appendChild(image3);
+      div4divider.appendChild(divider);
+    }
+
+
 }
 
 function searchProduct() {
